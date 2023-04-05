657b739a5bc49e5a9c85ed1ca9c7da2fac5dbed5
EJ <me@me>
2023-04-05 01:59:33 -0400

    Tile origin calculator and unit test

---
+++ Source/GridRuntime/Public/TileCalculator/SquareTileCalculator.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SquareTileCalculator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIDRUNTIME_API USquareTileCalculator : public UObject
+{
+	
+	GENERATED_BODY()
+
+public:
+	// result XY will always be positive
+	FVector2D GridToRelative(const FIntVector2 Coords, const double TileSize)
+	{
+		const auto HalfTile = TileSize / 2;
+		const auto BaseLine = FVector2D(
+			HalfTile,
+			HalfTile
+		);
+		const auto XYVector = BaseLine + FVector2D(TileSize, TileSize) * FVector2D(Coords.X, Coords.Y);
+		return XYVector;
+	}
+	
+};
---
+++ Source/GridRuntime/Public/TileCalculator/SquareTileCalculatorTest.cpp
@@ -0,0 +1,18 @@
+﻿#include "SquareTileCalculator.h"
+#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(SquareTileCalculatorTest, "GridRuntime.Public.TileCalculator.SquareTileCalculatorTest",
+                                 EAutomationTestFlags::EditorContext | EAutomationTestFlags::SmokeFilter)
+
+// TODO this needs to be moved to the private folder
+bool SquareTileCalculatorTest::RunTest(const FString& Parameters)
+{
+	TStrongObjectPtr Calculator(NewObject<USquareTileCalculator>());
+
+	FVector2D Result = Calculator->GridToRelative(FIntVector2(0, 0), 100);
+
+	TestEqual("Calculator should return a half tile at (0,0)", Result, FVector2D(50, 50));
+	
+	// Make the test pass by returning true, or fail by returning false.
+	return true;
+}
